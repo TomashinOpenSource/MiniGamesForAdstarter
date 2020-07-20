c08390d71e6688003fceafe0ff04268812c0f88c
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-20 11:31:00 +0300

    Game000_TestingWebViewWithClicker

---
+++ Game000_TestingWebViewWithClicker/TestMobileWebViewApp/Assets/Scripts/GameSceneScripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Text countText;
+    private int count;
+    
+    
+    void Start()
+    {
+        count = 0;
+        PrintCount(count);
+    }
+
+    private void PrintCount(int c)
+    {
+        countText.text = c.ToString();
+    }
+
+    public void Pressed()
+    {
+        count++;
+        PrintCount(count);
+    }
+}
