295c76c01900e71354b373d80bf89b2cf2f30e7d
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-20 12:22:33 +0300

    com.Bestcopm.Clickclickclick.apk

---
+++ Game003_SimpleClicker/Assets/Scripts/ClickerManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ClickerManager : MonoBehaviour
+{
+    public TMP_Text countText;
+    public int count;
+
+    void Start()
+    {
+        count = 0;
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void Clicked()
+    {
+        count++;
+        countText.text = count.ToString();
+    }
+}
