dfd4dfc4c0e4d8fd02fb873951bbd8fbf7c2efcc
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-21 15:25:54 +0300

    SetRandomColor()

---
+++ Game006_ColorSwitch/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public float jumpForce;
+    public Rigidbody2D circle;
+    public string currentColor;
+    public SpriteRenderer sr;
+    public Color blue, yellow, pink, purple;
+
+    private void Start()
+    {
+        jumpForce = 10f;
+        circle = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        SetRandomColor();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            circle.velocity = Vector2.up * jumpForce;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+    }
+
+    private void SetRandomColor()
+    {
+        int rand = Random.Range(0, 3);
+        switch (rand)
+        {
+            case 0:
+                currentColor = "Blue";
+                sr.color = blue;
+                break;
+            case 1:
+                currentColor = "Yellow";
+                sr.color = yellow;
+                break;
+            case 2:
+                currentColor = "Pink";
+                sr.color = pink;
+                break;
+            case 3:
+                currentColor = "Purple";
+                sr.color = purple;
+                break;
+        }
+    }
+}
---
+++ Game006_ColorSwitch/Assets/Scripts/RotationScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScript : MonoBehaviour
+{
+    public float rotationSpeed;
+
+    void Start()
+    {
+        rotationSpeed = 100f;
+    }
+
+    void Update()
+    {
+        transform.Rotate(0f, 0f, rotationSpeed * Time.deltaTime);
+    }
+}
