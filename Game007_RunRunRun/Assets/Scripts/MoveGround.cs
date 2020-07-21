3ce7dc2647adab983e39d861afc1325e5e8e63a6
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-21 17:05:05 +0300

    grounds[i].Translate

---
+++ Game007_RunRunRun/Assets/Scripts/MoveGround.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveGround : MonoBehaviour
+{
+    public float speed;
+    public List<Transform> grounds;
+
+    private void Update()
+    {
+        for (int i = 0; i < grounds.Count; i++)
+        {
+            grounds[i].Translate(-speed * Time.deltaTime, 0f, 0f);
+        }
+    }
+}
