45938c7ce4e786843c08665967e5051e601c35c5
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-21 17:49:39 +0300

    MoveGround

--- Game007_RunRunRun/Assets/Scripts/MoveGround.cs
+++ Game007_RunRunRun/Assets/Scripts/MoveGround.cs
@@ -6,12 +6,26 @@ public class MoveGround : MonoBehaviour
 {
     public float speed;
     public List<Transform> grounds;
+    private Transform first, second;
+
+    private void Start()
+    {
+        first = grounds[0];
+        second = grounds[1];
+    }
 
     private void Update()
     {
         for (int i = 0; i < grounds.Count; i++)
         {
             grounds[i].Translate(-speed * Time.deltaTime, 0f, 0f);
+
         }
+    }
+
+    public void ChangePos()
+    {
+        grounds[0] = second;
+        grounds[1] = first;
     }
 }
