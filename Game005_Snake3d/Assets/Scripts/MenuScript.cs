9ace908d7992efa9a97440d271a04595e5f19873
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-24 16:23:41 +0300

    com.Shander.MoveSnakeXboct_2.aab

---
+++ Game005_Snake3d/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public bool isWeb;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            PlayButtonPressed();
+        }
+    }
+
+    public void PlayButtonPressed()
+    {
+        if (Application.systemLanguage == SystemLanguage.Russian && isWeb == true) SceneManager.LoadScene("WebViewScene");
+        else SceneManager.LoadScene("GameScene");
+    }
+
+    public void ResetButtonPressed()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
