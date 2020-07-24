d7497c2868d8476ae9fb358fe2c81225750083be
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-24 12:25:32 +0300

    com.RunClub.RunFromEarth_0.2(2).aab

---
+++ Game004_FlappyBall/Assets/Scripts/GameFlappyManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameFlappyManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject pipes;
+
+    void Awake()
+    {
+        //pipes = Resources.Load<GameObject>("Pipes");
+    }
+
+    void Start()
+    {
+        StartCoroutine(GeneratePipes());
+    }
+
+    void Update()
+    {
+        //scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator GeneratePipes()
+    {
+        Vector2 position;
+        while (true)
+        {
+            position = transform.position;
+            position.x += 6.0F;
+            Instantiate(pipes, position, Quaternion.identity);
+            yield return new WaitForSeconds(2.0F);
+        }
+    }
+
+    public void ResetButtonPressed()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+}
