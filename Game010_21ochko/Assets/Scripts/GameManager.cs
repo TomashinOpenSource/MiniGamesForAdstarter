23f637323480964433991d7b83a771c0e09e1254
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-27 15:51:11 +0300

    21

---
+++ Game010_21ochko/Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private TMP_Text playerText, rivalText;
+    private int playerCount = 0, rivalCount = 0;
+
+    void Start()
+    {
+        playerText.text = playerCount.ToString();
+        rivalText.text = rivalCount.ToString();
+    }
+    
+    public void NextButtonPressed()
+    {
+        playerCount += Random.Range(1, 7);
+        playerText.text = playerCount.ToString();
+        if (playerCount >= 21) StopButtonPressed();
+    }
+    
+    public void StopButtonPressed()
+    {
+        rivalCount += Random.Range(17, 25);
+        rivalText.text = rivalCount.ToString();
+        StartCoroutine(Wait());
+    }
+
+    public void ResetButtonPressed()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    private IEnumerator Wait()
+    {
+        yield return new WaitForSeconds(2);
+        ResetButtonPressed();
+    }
+
+}
