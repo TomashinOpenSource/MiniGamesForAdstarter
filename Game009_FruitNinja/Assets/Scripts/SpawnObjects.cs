9f08d9798b37e036c69d597fd543922c9285c229
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-27 14:50:03 +0300

    cut fruits

---
+++ Game009_FruitNinja/Assets/Scripts/SpawnObjects.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnObjects : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject prefabToSpawn;
+
+    [SerializeField]
+    private float spawnInterval, objectMinX, objectMaxX, objectY;
+
+    [SerializeField]
+    private Sprite[] objectSprites;
+
+    void Start()
+    {
+        InvokeRepeating("spawnObject", this.spawnInterval, this.spawnInterval);
+    }
+
+    private void spawnObject()
+    {
+        GameObject newObject = Instantiate(this.prefabToSpawn);
+        newObject.transform.position = new Vector2(Random.Range(this.objectMinX, this.objectMaxX), this.objectY);
+
+        Sprite objectSprite = objectSprites[Random.Range(0, this.objectSprites.Length)];
+        newObject.GetComponent<SpriteRenderer>().sprite = objectSprite;
+    }
+}
