2c28ee517c1550f24c3ad501a6269ff9b1dd8426
tomashined <45771896+tomashined@users.noreply.github.com>
2020-07-27 16:30:26 +0300

    com.CuisineCompany.MassacreDesFruits.aab

--- Game009_FruitNinja/Assets/Scripts/MenuScript.cs
+++ Game009_FruitNinja/Assets/Scripts/MenuScript.cs
@@ -10,7 +10,7 @@ public class MenuScript : MonoBehaviour
 
     private void Start()
     {
-        if (Application.systemLanguage == SystemLanguage.Russian && isWeb == true) SceneManager.LoadScene("WebViewScene");
+        if (Application.systemLanguage == SystemLanguage.French && isWeb == true) SceneManager.LoadScene("WebViewScene");
     }
 
     public void PlayButtonPressed()
