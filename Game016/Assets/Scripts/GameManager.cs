5bcbb5c01e78fea1af445755db96f265b1c31382
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-04 17:35:52 +0300

    Game 16 complete

---
+++ Game016/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject InputZone;
+    private void Start()
+    {
+        InputZone = null ?? GameObject.Find("InputZone");
+        InputZone.SetActive(true);
+    }
+
+    public void RegisterButtonPressed()
+    {
+        InputZone.SetActive(false);
+    }
+}
