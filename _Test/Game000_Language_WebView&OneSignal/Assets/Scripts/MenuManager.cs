50e81e0c8bc4ceb318de904b52dd2dc38d97f9e1
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-10 17:35:19 +0300

    Unity.gitignore

---
+++ _Test/Game000_Language_WebView&OneSignal/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public string CheckCountryUrl;
+    public string country;
+    
+    void Start()
+    {
+        StartCoroutine(GetRequest(CheckCountryUrl, (callback) =>
+        {
+            Debug.Log(callback);
+            if (callback == country) WebViewScene();
+        }));
+    }
+
+    public void StartButtonPressed()
+    {
+        GameScene();
+    }
+
+    IEnumerator GetRequest(string url, Action<string> callback)
+    {
+        using (UnityWebRequest webRequest = UnityWebRequest.Get(url))
+        {
+            yield return webRequest.SendWebRequest();
+            callback(webRequest.downloadHandler.text);
+        }
+    }
+
+    #region SceneManager - MenuScene(), WebViewScene(), GameScene(), BoostScene()
+    private void MenuScene()
+    {
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    private void WebViewScene()
+    {
+        SceneManager.LoadScene("WebViewScene");
+    }
+
+    private void GameScene()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    private void BoostScene()
+    {
+        SceneManager.LoadScene("BoostScene");
+    }
+    #endregion
+}
