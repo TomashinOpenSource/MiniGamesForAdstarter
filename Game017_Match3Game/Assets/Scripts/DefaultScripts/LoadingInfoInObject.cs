e6ffc959237778358a2a80b7d403dee4747851a2
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-10 19:57:11 +0300

    Зарабатывай, выбирая 2+ в ряд

---
+++ Game017_Match3Game/Assets/Scripts/DefaultScripts/LoadingInfoInObject.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadingInfoInObject : MonoBehaviour
+{
+    public TMP_Text PercentText, LoadText;
+    public Slider slider;
+
+    private float _step;
+
+    private void Start()
+    {
+        slider.minValue = 0;
+        slider.maxValue = 100;
+        SetPercent(slider.minValue);
+        StartCoroutine(FillPercent());
+    }
+
+    private IEnumerator FillPercent()
+    {
+        do
+        {
+            slider.value += Random.Range(5, 10);
+            SetPercent(slider.value);
+            yield return new WaitForSeconds(0.1f);
+        } while (slider.value <= 100);
+    }
+
+    private void SetPercent(float p)
+    {
+        slider.value = p;
+        PercentText.text = p.ToString() + "%";
+    }
+}
--- Game017_Match3Game/Assets/Scripts/GameScripts/Board.cs
+++ Game017_Match3Game/Assets/Scripts/GameScripts/Board.cs
@@ -1,17 +1,21 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
     public int width, height, borderSize;
-    public GameObject tilePrefab;
+    public List<GameObject> characters;
 
     Tile[,] m_allTiles;
 
     void Start()
     {
+        width = Random.Range(2, 10);
+        height = Random.Range(2, 10);
+        borderSize = Random.Range(0, 3);
         m_allTiles = new Tile[width, height];
         SetupTiles();
         SetupCamera();
@@ -23,7 +27,7 @@ public class Board : MonoBehaviour
         {
             for (int j = 0; j < height; j++)
             {
-                GameObject tile = Instantiate(tilePrefab, new Vector3(i, j, 0), Quaternion.identity) as GameObject;
+                GameObject tile = Instantiate(characters[Random.Range(0, characters.Count)], new Vector3(i, j, 0), Quaternion.identity) as GameObject;
                 tile.name = "Tile (" + i + "," + j + ")";
                 m_allTiles[i, j] = tile.GetComponent<Tile>();
                 tile.transform.parent = transform;
@@ -39,5 +43,10 @@ public class Board : MonoBehaviour
         float verticalSize = (float)height / 2f + (float)borderSize;
         float horizontalSize = ((float)width / 2f + (float)borderSize) / aspectRatio;
         Camera.main.orthographicSize = (verticalSize > horizontalSize) ? verticalSize : horizontalSize;
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("GameScene");
     }
 }
