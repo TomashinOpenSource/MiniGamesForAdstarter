3941126084fc90398a2129f6bce71a43ae05f380
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-12 16:48:25 +0300

    Game 22 complete

---
+++ Game022_JumpForCristall/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject Pirat;
+    public int force;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Pirat.GetComponent<Rigidbody2D>().AddForce(Vector2.up * (force), ForceMode2D.Impulse);
+        }
+    }
+
+    public void SpawnCristall()
+    {
+
+    }
+}
