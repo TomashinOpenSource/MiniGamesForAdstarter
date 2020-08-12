9ea06579aff4a7b14c7c513f23d77b42ea099acf
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-12 12:44:34 +0300

    Game020_TapForChangeSprite

---
+++ Game020_TapForChangeSprite/Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject GameObject;
+    public Transform Field;
+    public List<Sprite> Sprites;
+
+
+    void Start()
+    {
+        SpawnTiles();
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void SpawnTiles()
+    {
+        for (int i = 0; i < 32; i++)
+        {
+            GameObject _g = Instantiate(GameObject, Field);
+            _g.GetComponent<Image>().sprite = Sprites[Random.Range(0, Sprites.Count)];
+        }
+    }
+
+}
---
+++ Game020_TapForChangeSprite/Assets/Scripts/Tile.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Tile : MonoBehaviour
+{
+    private GameManager GameManager;
+
+    private void Awake()
+    {
+        GameManager = GameObject.FindObjectOfType<GameManager>();
+    }
+
+    public void Pressed()
+    {
+        this.GetComponent<Image>().sprite = GameManager.Sprites[Random.Range(0, GameManager.Sprites.Count)];
+    }
+}
