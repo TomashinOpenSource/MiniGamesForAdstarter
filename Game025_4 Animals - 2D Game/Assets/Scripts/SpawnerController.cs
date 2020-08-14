2aca02912f77300c89376e7d5cbfb2e2b3cc925e
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-14 15:01:18 +0300

    Game 25

---
+++ Game025_4 Animals - 2D Game/Assets/Scripts/SpawnerController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SpawnerController : MonoBehaviour {
+
+    public GameObject[] Fruits;
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnFruits());
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    private IEnumerator SpawnFruits()
+    {
+        GameObject _object = Instantiate(Fruits[Random.Range(0, Fruits.Length)]);
+
+        Vector2 SpawnPos = new Vector2(Random.Range(-2.5f, 2.5f), 6f);
+        _object.transform.position = SpawnPos;
+
+
+        yield return new WaitForSeconds(Random.Range(0f, 3f));
+
+        StartCoroutine(SpawnFruits());
+
+        yield return new WaitForSeconds(100f);
+        Destroy(_object);
+    }
+
+}
