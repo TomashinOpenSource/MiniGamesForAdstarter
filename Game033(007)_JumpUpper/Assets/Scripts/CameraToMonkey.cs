399e06d73594d48d98b9e864525ddec5a5b7eb70
tomashined <45771896+tomashined@users.noreply.github.com>
2020-08-28 11:55:20 +0300

    camera moving

---
+++ Game033(007)_JumpUpper/Assets/Scripts/CameraToMonkey.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraToMonkey : MonoBehaviour
+{
+    public Transform MonkeyTarget;
+    float zOffset;
+    private void Start()
+    {
+        zOffset = transform.position.z;
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, MonkeyTarget.position, Time.deltaTime * 5);
+        transform.position = new Vector3(transform.position.x, transform.position.y, zOffset);
+    }
+}
---
+++ Game033(007)_JumpUpper/Assets/Scripts/Jumping.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jumping : MonoBehaviour
+{
+    private Rigidbody2D rigidbody;
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rigidbody.velocity += Vector2.up * Random.Range(10, 20);
+        }
+    }
+}
